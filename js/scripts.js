59c947543fadd662772ba73fa3dedd5574128309
Sploob <crayiphone@gmail.com>
2022-03-23 16:21:45 -0700

    add ability to remove contact using a removal button, and extra form fields
    Co-authored-by: Gabe Nielsen <gabenielsen7@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -6,11 +6,17 @@ $(document).ready(function() {
     const city = $("#city").val();
     const state = $("#state").val();
     const zipcode = $("#zip").val();
+    const phone = $("#phone").val();
+    const email = $("#email").val();
     
     let contact = $("<ul></ul>");
+    let button = $("<button type='button' class='btn-close btn-danger' aria-label='Close'></button>");
     contact.html(name);
+    contact.append(button);
     contact.append($("<li>" + streetAddress + "</li>"));
     contact.append($("<li>" + city + ", " + state + " " + zipcode +"</li>"));
+    contact.append($("<li>" + phone + "</li>"));
+    contact.append($("<li>" + email + "</li>"));
 
     $("#address-book").prepend(contact);
 
@@ -18,10 +24,15 @@ $(document).ready(function() {
         $(this).children().toggleClass("hidden");
       });
 
-    contact.contextmenu(function(event) {
-      event.preventDefault();
-      $(this).remove();
-    });
+    $(".btn-close").click(function() {
+        $(this).parent().remove();
+    }); 
+
+    // Right click to remove contact
+    // contact.contextmenu(function(event) {
+    //   event.preventDefault();
+    //   $(this).remove();
+    // });
   });
 
 });
